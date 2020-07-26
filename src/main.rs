56b327a3d14e7ed05a95b760cc93e159f1654d99
dekokun <dekokun@hatena.ne.jp>
2020-07-26 22:02:28 +0900

    sqlx example

--- src/main.rs
+++ src/main.rs
@@ -15,18 +15,27 @@ async fn main() -> Result<(), sqlx::Error> {
     Ok(())
 }
 
+fn print_typename<T>(_: T) {
+    println!("{}", std::any::type_name::<T>());
+}
+
 async fn sqlx() -> Result<(), sqlx::Error> {
     use sqlx::prelude::*;
     use sqlx::*;
     let mut conn = MySqlConnection::connect("mysql://isucon:isucon@localhost:3306/isucon").await?;
-
-    let row: (i64,) = sqlx::query_as("SELECT ?")
-        .bind(150_i64)
-        .fetch_one(&mut conn)
+    let _insert_count = sqlx::query!("INSERT INTO payment (customer_id, amount, account_name) VALUES (1, 1, 'hoge1'), (2, 2, null), (3, 3, null)").execute(&mut conn).await?;
+    let fetch_all_rows = sqlx::query!("SELECT * from payment where customer_id = ?", 1)
+        .fetch_all(&mut conn)
         .await?;
 
-    assert_eq!(row.0, 150);
+    assert_eq!(fetch_all_rows[0].customer_id, 1);
+    assert_eq!(fetch_all_rows[0].amount, 1);
+    assert_eq!(fetch_all_rows[0].account_name.as_ref().unwrap(), "hoge1");
 
+    let fetch_one_row = sqlx::query!("SELECT * from payment where customer_id = ?", 1)
+        .fetch_one(&mut conn)
+        .await?;
+    assert_eq!(fetch_all_rows[0].amount, fetch_one_row.amount);
     Ok(())
 }
 
