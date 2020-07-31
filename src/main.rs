536cc2856bb3389f52991f85fc6c8a6b77c3c93d
dekokun <dekokun@hatena.ne.jp>
2020-07-31 22:02:49 +0900

    use redis crate

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,30 @@ async fn main() -> Result<(), sqlx::Error> {
     let ret = sqlx().await;
 
     println!("{:?}", ret);
+
+    let ret = redis();
+    println!("{:?}", ret);
     Ok(())
 }
 
 fn print_typename<T>(_: T) {
     println!("{}", std::any::type_name::<T>());
+}
+
+fn redis() -> redis::RedisResult<()> {
+    use redis::Commands;
+    let client = redis::Client::open("redis://127.0.0.1/")?;
+    let mut con = client.get_connection()?;
+    con.set("my_key", 42)?;
+    let count: i32 = con.get("my_key")?;
+    assert_eq!(count, 42);
+
+    con.set("my_key", "hoge")?;
+    let count: i32 = con.get("my_key").unwrap_or(0);
+    assert_eq!(count, 0);
+
+    // use redis sorted set
+    Ok(())
 }
 
 async fn sqlx() -> Result<(), sqlx::Error> {
