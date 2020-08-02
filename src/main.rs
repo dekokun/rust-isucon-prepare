055197fa6932df9e57d96b73f394bb0f06224dc8
dekokun <dekokun@hatena.ne.jp>
2020-08-02 20:26:23 +0900

    use lazy static

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
+use lazy_static::lazy_static;
 mod mysql_sample;
 mod redis_sample;
 mod sqlx_sample;
 
+use std::collections::HashMap;
+
+lazy_static! {
+    static ref HASHMAP: HashMap<u32, &'static str> = {
+        let mut m = HashMap::new();
+        m.insert(0, "foo");
+        m.insert(1, "bar");
+        m.insert(2, "baz");
+        m
+    };
+}
+
 #[async_std::main]
 async fn main() {
+
+    println!("{}", HASHMAP.get(&0).unwrap())
+
     let ret = mysql_sample::mysql().expect("failed mysql crate sample");
     println!("{:?}", ret);
+
     let ret = sqlx_sample::sqlx().await.expect("failed sqlx crate sample");
 
     println!("{:?}", ret);
