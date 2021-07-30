a7bcb6ad152ea906bb2e688dc08f3d11f5f8501a
dekokun <dekokun@hatena.ne.jp>
2021-07-30 21:45:45 +0900

    add redis test

--- src/redis_sample.rs
+++ src/redis_sample.rs
@@ -9,6 +9,11 @@ pub fn redis() -> redis::RedisResult<()> {
     con.set("my_key", "hoge")?;
     let count: i32 = con.get("my_key").unwrap_or(0);
     assert_eq!(count, 0);
+    con.set("my_key", "hoge")?;
+    let my_key: String = con.get("my_key").unwrap_or("fuga".into());
+    assert_eq!(my_key, "hoge");
+    let my_key: String = con.get("not_exists").unwrap_or("fuga".into());
+    assert_eq!(my_key, "fuga");
 
     con.zadd("rank", "1deko", 1)?;
     con.zadd("rank", "2deko", 2)?;
